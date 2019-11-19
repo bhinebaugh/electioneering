c3b4120aab77877242125412c162d9c044847969
Byron Hinebaugh <bhinebaugh@gmail.com>
2019-11-18 21:13:18 -0500

    add deckbuilder library, basic deck init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Deckbuilder from 'deckbuilder';
 import './App.css';
 
+function prepareCards(deck) {
+  for (let id=1; id<7; id++) {
+    deck.add({
+      id,
+      name: Math.random().toString(36).substring(2,10)
+    });
+  }
+  deck.shuffle();
+  deck.draw(3);
+}
+
 function App() {
+  const deck = new Deckbuilder();
+  prepareCards(deck);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      Cards:
+      <ul>
+        {deck.drawn.map( card => <li>{card.name}</li>)}
+      </ul>
     </div>
   );
 }
