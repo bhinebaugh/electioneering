6fc05b87c595f19559e503813f8d291f493f7757
Byron Hinebaugh <466561+bhinebaugh@users.noreply.github.com>
2020-06-14 14:43:41 -0400

    style results screen

--- src/FinalResult.js
+++ src/FinalResult.js
@@ -1,22 +1,32 @@
 import React from 'react'
 import './final.css'
 
-const FinalResult = ({ candidates }) => (
-    <div id="final">
-        <h1>Election Day!</h1>
-        <p>The game is over. Democracy wins!</p>
-        <table>
-            <tr><th>name</th><th>polling</th><th>turnout modifier</th><th>votes</th></tr>
-            {candidates.map(candidate =>
-                <tr>
-                    <td>{candidate.name}</td>
-                    <td>{candidate.stats.polling}</td>
-                    <td>1.00</td>
-                    <td>{candidate.stats.polling * 1014 }</td>
-                </tr>
-            )}
-        </table>
-    </div>
-)
+const FinalResult = ({ candidates }) => {
+    const plurality = candidates.reduce( (p, c, i) => c > p ? c : p, 0)
+    const winner = candidates.reduce((a,b) => a.polling > b.polling ? a.id : b.id)
+    return(
+        <div id="overlay">
+        <section id="final">
+            <header>
+                <h1>Election Day Results</h1>
+            </header>
+            <div>
+                <p>Polls have closed and votes have been tallied</p>
+                <table>
+                    <tr><th>name</th><th>polling</th><th>turnout modifier</th><th>votes</th></tr>
+                    {candidates.map(candidate =>
+                        <tr className={candidate.id == winner ? "winner" : "also-ran"}>
+                            <td>{candidate.name}</td>
+                            <td>{candidate.stats.polling}%</td>
+                            <td>1.00</td>
+                            <td>{(candidate.stats.polling * 1014).toLocaleString() }</td>
+                        </tr>
+                    )}
+                </table>
+            </div>
+        </section>
+        </div>
+    )
+}
 
 export default FinalResult
